589508b5878d289fe0f54d16f902a058839c37ce
Baruch Sadogursky <jbaruch@jfrog.com>
2014-10-11 13:25:49 +0300

    Create JpaConfig.java

---
+++ src/main/java/conference/JpaConfig.java
@@ -0,0 +1,50 @@
+package conference;
+
+import org.apache.derby.jdbc.EmbeddedDriver;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import java.util.Properties;
+
+/**
+ * @author Jeka
+ * @since 07/10/2014
+ */
+@Configuration
+@ComponentScan
+public class JpaConfig {
+    @Bean
+    public DriverManagerDataSource conferenceDataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(EmbeddedDriver.class.getName());
+        dataSource.setUrl("jdbc:derby:conference;create=true");
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
+        LocalContainerEntityManagerFactoryBean emFactory = new LocalContainerEntityManagerFactoryBean();
+        emFactory.setDataSource(conferenceDataSource());
+        emFactory.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+        emFactory.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        Properties jpaProperties = new Properties();
+        jpaProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.DerbyTenSevenDialect");
+        jpaProperties.setProperty("hibernate.hbm2ddl.auto", "create");
+        emFactory.setJpaProperties(jpaProperties);
+        emFactory.setPackagesToScan("conference");
+        return emFactory;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(){
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+        return transactionManager;
+    }
+}
